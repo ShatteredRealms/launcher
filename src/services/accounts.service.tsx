ce357012a1acfe915088d6e2b51ddb755cc92676
Wil Simpson <simpson.wil255@gmail.com>
2022-09-27 11:27:42 -0500

    Set default api to production if none set

--- src/services/accounts.service.tsx
+++ src/services/accounts.service.tsx
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
-let ACCOUNTS_API = process.env.ACCOUNTS_API || '';
+let ACCOUNTS_API = '';
 
 window.electron.ipcRenderer.on('accounts-api-url', (resp) => {
   if (typeof resp === 'string') {
     ACCOUNTS_API = resp;
+  } else {
+    ACCOUNTS_API = 'https://api.shatteredrealmsonline.com/accounts';
   }
 });
 
